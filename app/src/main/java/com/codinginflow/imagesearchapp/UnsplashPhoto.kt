8bc6cca8d12f11cbbbc38ba3b6bfd205b281d223
zansangeeth <zansangeeth17@gmail.com>
2022-07-21 04:25:42 +0530

    created UnsplashPhoto.kt file for photo reference

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,35 @@
+package com.codinginflow.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ **@Project -> Image Search App
+ **@Author -> Sangeeth on 7/21/2022
+ */
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String,
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String,
+    ) : Parcelable {
+        val attributeUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
+    }
+}
