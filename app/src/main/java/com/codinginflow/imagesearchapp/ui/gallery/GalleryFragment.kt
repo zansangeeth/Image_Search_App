95d521386475eaddc075c8e344166d574908aa45
zansangeeth <zansangeeth17@gmail.com>
2022-07-21 16:13:11 +0530

    created nav_graph.xml

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,8 +1,12 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
 /**
  **@Project -> Image Search App
  **@Author -> Sangeeth on 7/21/2022
  */
-class GalleryFragment {
+class GalleryFragment : Fragment(R.layout.fragment_gallery){
+
 }
