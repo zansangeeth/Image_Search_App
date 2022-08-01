3d04b10c50db8b3fb4af48e7307ad1e9091fedea
zansangeeth <zansangeeth17@gmail.com>
2022-08-01 22:57:04 +0530

    added retry button when internet was failed to connect

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,13 +1,14 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
-import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.codinginflow.imagesearchapp.R
 import com.codinginflow.imagesearchapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,10 +18,10 @@ import dagger.hilt.android.AndroidEntryPoint
  **@Author -> Sangeeth on 7/21/2022
  */
 @AndroidEntryPoint
-class GalleryFragment : Fragment(R.layout.fragment_gallery){
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
-    private var _binding : FragmentGalleryBinding? = null
+    private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,15 +32,38 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery){
 
         binding.apply {
             rvGallery.setHasFixedSize(true)
+            rvGallery.itemAnimator = null
             rvGallery.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = UnsplashPhotoLoadStateAdapter {adapter.retry()},
-                footer = UnsplashPhotoLoadStateAdapter {adapter.retry()},
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
             )
+            btnRetry.setOnClickListener {
+                adapter.retry()
+            }
 
         }
 
-        viewModel.photos.observe(viewLifecycleOwner){
+        viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                rvGallery.isVisible = loadState.source.refresh is LoadState.NotLoading
+                btnRetry.isVisible = loadState.source.refresh is LoadState.Error
+                tvError.isVisible = loadState.source.refresh is LoadState.Error
+
+                //empty view
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1){
+                    rvGallery.isVisible = false
+                    tvEmpty.isVisible = true
+                }else{
+                    tvEmpty.isVisible = false
+                }
+
+            }
         }
         setHasOptionsMenu(true)
     }
@@ -51,9 +75,9 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery){
         val searchItem = menu.findItem(R.id.actionSearch)
         val searchView = searchItem.actionView as SearchView
 
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null){
+                if (query != null) {
                     binding.rvGallery.scrollToPosition(0)
                     viewModel.searchPhotos(query)
                     searchView.clearFocus()
